2d839457742b906595af0da5322a1aa5a1bb7b44
Anton Samartsev <samarcev.anton1@gmail.com>
2022-03-14 21:55:00 +0300

    manage.py improved

--- manage.py
+++ manage.py
@@ -1,33 +1,32 @@
 from config import *
-from startup.development import  development
-from startup.production import production
-from startup.test import test
-from startup.stage import stage
-from startup.init import init
-from startup.defualt import default
-from startup.sandbox import sandbox
-
 
 def start():
     if startup_mode == 'development':
+        from startup.development import development
         development(config[startup_mode])
 
     elif startup_mode == 'production':
+        from startup.production import production
         production(config[startup_mode])
 
     elif startup_mode == 'test':
+        from startup.test import test
         test(config[startup_mode])
 
     elif startup_mode == 'stage':
+        from startup.stage import stage
         stage(config[startup_mode])
 
     elif startup_mode == 'init':
+        from startup.init import init
         init(config[startup_mode])
 
     elif startup_mode == 'sandbox':
+        from startup.sandbox import sandbox
         sandbox(config[startup_mode])
 
     else:
+        from startup.defualt import default
         default()
 
 
